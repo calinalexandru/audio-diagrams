f3c6f5360d1da464e0db479087c9d8fad7f359d5
Alexandru Calin <calinalexandru1990@gmail.com>
2022-10-18 15:36:19 +0300

    fix: eslint config

--- src/primitives/Line.js
+++ src/primitives/Line.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import { h } from 'preact';
+/* @jsx h */
+import { h, } from 'preact';
 
 const LineStyled = styled.div`
   height: 1px;
@@ -25,13 +26,13 @@ const LineStyled = styled.div`
 
 const OFFSET_LEFT = 18;
 const OFFSET_HYPOTHENUSE = -18;
-export default function Line({ from: ogFrom, to: ogTo, onClick }) {
-  let from = ogFrom;
-  let to = ogTo;
+export default function Line({ from: ogFrom, to: ogTo, onClick, },) {
+  const from = ogFrom;
+  const to = ogTo;
 
-  const heightRaw = Number(to.height.replace('px', ''));
-  const fromHeightRaw = Number(from.height.replace('px', ''));
-  const leftRaw = from.x + Number(from.width.replace('px', ''));
+  const heightRaw = Number(to.height.replace('px', '',),);
+  const fromHeightRaw = Number(from.height.replace('px', '',),);
+  const leftRaw = from.x + Number(from.width.replace('px', '',),);
   const widthRaw = to.x - leftRaw;
   const left = `${leftRaw + OFFSET_LEFT}px`;
   const top = `${from.y + fromHeightRaw / 2}px`;
@@ -40,10 +41,10 @@ export default function Line({ from: ogFrom, to: ogTo, onClick }) {
   // tangent angle = atan / tan^2 -1
   const c1 = from.y - to.y;
   const c2 = widthRaw;
-  const hypothen = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+  const hypothen = Math.sqrt(c1 ** 2 + c2 ** 2,);
   // console.log(`c1: ${c1}, c2: ${c2}, h: ${hypothen}`);
-  const angle = ((Math.atan2(c1, c2) * 180) / Math.PI) * -1;
-  const width = `${hypothen + OFFSET_HYPOTHENUSE}px`; //`${widthRaw}px`;
+  const angle = ((Math.atan2(c1, c2,) * 180) / Math.PI) * -1;
+  const width = `${hypothen + OFFSET_HYPOTHENUSE}px`; // `${widthRaw}px`;
 
   // console.log('angle', angle);
 
